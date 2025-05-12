3e860781d4f700c298933f056fe9b7d55b29d5f7
Israel Castro <94873358+IsraelCtr@users.noreply.github.com>
2025-05-11 22:41:57 -0300

    Update admin.jsx

--- primeira-aplicacao-react/src/components/admin/admin.jsx
+++ primeira-aplicacao-react/src/components/admin/admin.jsx
@@ -2,42 +2,35 @@ import "./estiloAdmin.css"
 import admim from '../../assets/adm.png'
 import { useState } from "react"
 
-export const Admin =()=>{
-    
+export const Admin = () => {
     const [textoCopiado, setTextoCopiado] = useState(false);
 
-  const copiarTexto = () => {
-    const texto = "israel.castro.imc@gmail.com";
-    navigator.clipboard.writeText(texto)
-      .then(() => {
-        setTextoCopiado(true); // Exibe a notificação
-        setTimeout(() => {
-          setTextoCopiado(false); // Remove a notificação após 2 segundos
-        }, 2000);
-      })
-      .catch(err => {
-        console.error('Erro ao copiar texto: ', err);
-      });
-  };
-    return(
-<div className="contentAdmin">
-    <div className="contentText">
-        <h1 id="apresentation">Hey i'm Israel</h1>
-        
-        <h1 className="tec"> Full-Stack Developer</h1>
-        
-        <button onClick={copiarTexto}id="Cv" >Send-me E-mail</button>
-        
-        
-    </div>
+    const copiarTexto = () => {
+        const texto = "israel.castro.imc@gmail.com";
+        navigator.clipboard.writeText(texto)
+            .then(() => {
+                setTextoCopiado(true); // Altera o estado para mostrar que o texto foi copiado
+                setTimeout(() => {
+                    setTextoCopiado(false); // Volta ao estado original após 2 segundos
+                }, 2000);
+            })
+            .catch(err => {
+                console.error('Erro ao copiar texto: ', err);
+            });
+    };
 
-    <div className="contenImg">
-
-            <img src={admim} className="admin" alt='background app'/>
-    </div>
-  
-
-</div>
-
-)
+    return (
+        <div className="contentAdmin">
+            <div className="contentText">
+                <h1 id="apresentation">Hey i'm Israel</h1>
+                <h1 className="tec"> Full-Stack Developer</h1>
+                <button onClick={copiarTexto} id="Cv">
+                    {textoCopiado ? "E-mail Copied!" : "Send-me E-mail"}
+                </button>
+            </div>
+            <div className="contenImg">
+                <img src={admim} className="admin" alt='background app'/>
+            </div>
+        </div>
+    )
 }
